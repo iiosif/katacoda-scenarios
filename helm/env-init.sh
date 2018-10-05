0584af7bef645108e110c106f22ff4d22791b315
iigna <31695453+iiosif@users.noreply.github.com>
2018-10-05 22:54:46 +0300

    update index.json for helm

--- helm/env-init.sh
+++ helm/env-init.sh
@@ -1,1 +1,1 @@
-ssh root@host01 "curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash; kubectl create -f rbac-config.yaml;"
+ssh root@host01 "curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash; git clone https://github.com/sgandon/katacoda-scenarios.git; mv katacoda-scenarios/kubernetes/assets/ .; rm -rf katacoda-scenarioskubectl; kubectl create -f rbac-config.yaml;"
